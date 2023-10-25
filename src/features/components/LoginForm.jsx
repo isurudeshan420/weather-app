5af82cf6b2de4bd8b1d990f3409d82d3c6ab8837
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2023-10-25 16:39:25 +0530

    login form init

--- src/features/components/LoginForm.jsx
+++ src/features/components/LoginForm.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './LoginForm.css';
 
 function LoginForm() {
   return (
-    <div>loginform</div>
-  )
+    <div className="loginForm">
+      <div className="columnItems">
+        <div className="loginLabel">
+          <h1>Login</h1>
+        </div>
+        <div className="unameInput">
+          <input
+            type="text"
+            placeholder="Username"
+            // Add any necessary props or state here
+          />
+        </div>
+        <div className="pwInput">
+          <input
+            type="password"
+            placeholder="Password"
+            // Add any necessary props or state here
+          />
+        </div>
+        <div>
+          <button type="submit">Log In</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
