c6f1c0a33464d6cfab796c98cadb53c4cbb5e0bf
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2023-10-25 16:35:32 +0530

    login page init

--- src/features/pages/LoginPage.jsx
+++ src/features/pages/LoginPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+
 import LoginForm from '../components/LoginForm';
 import "./LoginPage.css";
 
 function LoginPage() {
   return (
-    <div>
+    <div className="loginPage">
       <h1>Login Page</h1>
       <LoginForm />
     </div>
