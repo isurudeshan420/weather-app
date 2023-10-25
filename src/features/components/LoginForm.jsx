d68ec59dd93fe12bd7e675d0a839243a491098d2
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2023-10-25 23:40:15 +0530

    login final

--- src/features/components/LoginForm.jsx
+++ src/features/components/LoginForm.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
-import './LoginForm.css';
+import React, { useState } from "react";
+import "./LoginForm.css";
+import users from "../../assets/users.json";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(null);
+
+  const handleLogin = () => {
+    setLoginError(null);
+
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
+    try {
+      if (user) {
+        // Authentication successful
+        console.log("Authentication successful");
+      } else {
+        // Authentication failed
+        setLoginError("Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Error loading user data:", error);
+      setLoginError("An error occurred. Please try again later.");
+    }
+  };
+
   return (
     <div className="loginForm">
       <div className="columnItems">
@@ -12,19 +37,22 @@ function LoginForm() {
           <input
             type="text"
             placeholder="Username"
-            // Add any necessary props or state here
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="pwInput">
           <input
             type="password"
             placeholder="Password"
-            // Add any necessary props or state here
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div>
-          <button type="submit">Log In</button>
+          <button onClick={handleLogin}>Log In</button>
         </div>
+        {loginError && <p className="error">{loginError}</p>}
       </div>
     </div>
   );
